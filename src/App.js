b27b7fc14ec03f8248a82664f0179f8100bc9d32
Kshitiz <kshitiz.stha11@gmail.com>
2021-09-26 21:55:06 -0400

    using styled component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const App = () => {
+  const BtnStyle = styled.button`
+    background: blue;
+    color: white;
+  `;
   return (
     <React.Fragment>
-      <button className="btn btn-danger">click me</button>
+      <BtnStyle>click me</BtnStyle>
     </React.Fragment>
   );
 };
