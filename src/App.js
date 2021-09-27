23297acaf70fae22d14cfd58e12b0c9f9e760da5
Kshitiz <kshitiz.stha11@gmail.com>
2021-09-26 21:53:13 -0400

    using react btstrp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
 const App = () => {
-  const mybtn = {
-    background: 'red',
-    padding: '1rem 0rem',
-  };
-
   return (
     <React.Fragment>
-      <button style={mybtn}>click me</button>
+      <button className="btn btn-danger">click me</button>
     </React.Fragment>
   );
 };
