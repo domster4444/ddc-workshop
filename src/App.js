2cc5e548314cff753b15050de9206bcfaddf7bad
Kshitiz <kshitiz.stha11@gmail.com>
2021-09-27 15:22:56 -0400

    using react icons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import myImage from './assets/jptimage.jpeg';
-import mySvg from './assets/panda.svg';
-import myVdo from './assets/React in 100 Seconds.mp4';
+import { BiCartAlt } from 'react-icons/bi';
 const App = () => {
   return (
     <div>
-      <video width="320" height="240" controls>
-        <source src={myVdo} type="video/mp4" />
-      </video>
-      <img src={myImage} alt="" />
-      <img src={mySvg} alt="" />
+      <BiCartAlt style={{ fontSize: '5rem' }} />
     </div>
   );
 };
