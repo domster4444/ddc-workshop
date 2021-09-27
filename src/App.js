dd4cc25143acbf99c8261199df0437853ca30554
Kshitiz <kshitiz.stha11@gmail.com>
2021-09-27 15:30:16 -0400

    using list in react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
+// list in react
 import React from 'react';
+
 const App = () => {
-  const alertFunc = () => {
-    alert('triggered');
-  };
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const newArr = arr.map((num, index) => {
+    return <li key={index}>{num}</li>;
+  });
   return (
     <div>
-      <button onClick={alertFunc}>click me</button>
-      <button onMouseOver={alertFunc}>mouseOver me</button>
-      <button onMouseEnter={alertFunc}>mouseEnter me</button>
-      <button onMouseLeave={alertFunc}>mouseLeave me</button>
+      <ul>{newArr}</ul>
     </div>
   );
 };
