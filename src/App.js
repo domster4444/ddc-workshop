7b2d273e3a9af67c99ffd5a08a45f5d20613c2c7
Kshitiz <kshitiz.stha11@gmail.com>
2021-09-27 15:19:10 -0400

    using assets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import myImage from './assets/jptimage.jpeg';
+import mySvg from './assets/panda.svg';
+import myVdo from './assets/React in 100 Seconds.mp4';
 const App = () => {
   return (
-    <React.Fragment>
-      <Button color="primary" variant="contained">
-        press me
-      </Button>
-    </React.Fragment>
+    <div>
+      <video width="320" height="240" controls>
+        <source src={myVdo} type="video/mp4" />
+      </video>
+      <img src={myImage} alt="" />
+      <img src={mySvg} alt="" />
+    </div>
   );
 };
 
