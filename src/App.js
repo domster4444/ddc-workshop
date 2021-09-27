805a57c7a2b0fdd0499922e61476969207bd68c5
Kshitiz <kshitiz.stha11@gmail.com>
2021-09-27 15:47:30 -0400

    using react router with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React, { useState } from 'react';
-import Button from './Button';
+import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import User from './pages/User';
+import Login from './pages/Login';
 const App = () => {
-  const [state, setState] = useState(0);
-  const isLoggedIn = false;
   return (
-    <div>
-      <i>
-        <b>{`${state}`}</b>
-      </i>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/about">about</Link>
+          </li>
+          <li>
+            <Link to="/user">user</Link>
+          </li>
+          <li>
+            <Link to="/login">login</Link>
+          </li>
+        </nav>
+      </header>
 
-      <br />
-
-      {isLoggedIn ? <button> log out</button> : <button>log in</button>}
-
-      <u>my btn component</u>
-      <Button></Button>
-    </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about" component={About} />
+        <Route
+          path="/user"
+          component={() => {
+            return <User />;
+          }}
+        />
+        <Route exact path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
